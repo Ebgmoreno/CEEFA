2dd57d7231fed53bd74da3039a35038daa89d8bd
Brandon <ebgmoreno@gmail.com>
2024-10-17 20:36:00 -0600

    Sin boton de equipos

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import { provideRouter, Routes } from '@angular/router';
 import { HomeComponent } from './app/home/home.component';
 import { FormularioEquipoComponent } from './app/formulario-equipo/formulario-equipo.component';
 import { LoginComponent } from './app/usuarios/login/login.component';
-import { EquiposRegistradosComponent } from './app/equipos-registrados/equipos-registrados.component';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'formulario', component: FormularioEquipoComponent },
   { path: '', component: LoginComponent },
-  { path: 'equipos-registrados', component: EquiposRegistradosComponent } 
 
 ];
 
